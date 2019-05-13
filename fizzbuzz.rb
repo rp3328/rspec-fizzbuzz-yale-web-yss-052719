83840407327afbd8cfe82aae58ee534128d43472
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-13 03:10:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  else 
+    return    
+  end
 end
